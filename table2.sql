3380d251499a4a62951badf2946f994d49975c3f
rajshreeee <rajshreerai931@gmail.com>
2020-01-01 21:52:33 +0545

    add table2

---
+++ table2.sql
@@ -0,0 +1,15 @@
+--------------------------------------------------------
+--  File created - Wednesday-January-01-2020   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table TABLE1
+--------------------------------------------------------
+
+  CREATE TABLE "DS"."TABLE1" 
+   (	"COLUMN1" VARCHAR2(20 BYTE) COLLATE "USING_NLS_COMP"
+   )  DEFAULT COLLATION "USING_NLS_COMP" SEGMENT CREATION DEFERRED 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  TABLESPACE "USERS" ;
+REM INSERTING into DS.TABLE1
+SET DEFINE OFF;
